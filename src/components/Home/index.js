340066a9fcdf519a661f9be957497591fe63ad76
vishalsingh2710 <rvishal2710@gmail.com>
2023-06-26 19:19:35 +0000

    initial message

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home-img"
+    />
+    <h1 className="home-heading">Home</h1>
+  </div>
+)
+
+export default Contact
